488f46bf4c389e0fa586e2fc24adedbdddd60e26
lambrosopos <lambrosopos@gmail.com>
2020-01-16 01:01:41 +0900

    Changed first_app to index, added static images

--- first_project/second_app/views.py
+++ first_project/second_app/views.py
@@ -3,4 +3,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
+    
     return HttpResponse("<em>Hello World Emphasis</em>")
+
+
+def help(request):
+  help_dictionary = {
+    'insert_help' : "What would  you like help with?"
+  }
+  return render(request, 'second_app/help.html', context=help_dictionary)
