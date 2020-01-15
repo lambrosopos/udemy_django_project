899d88a9c646dc4b3cd56d88680f2745e37d8160
lambrosopos <lambrosopos@gmail.com>
2020-01-15 21:50:27 +0900

    Added templates folder and linked it with first_app

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -5,4 +5,5 @@ from django.shortcuts import render
 from  django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello World!")
+    my_dictionary = {'insert_me': "Hello, this is views.py"}
+    return render(request, 'first_app/index.html', context=my_dictionary)
