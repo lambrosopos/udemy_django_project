7a6249e425a4dda6488f63c9885091e3ed6c855a
lambrosopos <lambrosopos@gmail.com>
2020-01-16 16:19:31 +0900

    Revealing data on website through django

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from first_app.models import AccessRecord, Topic, Webpage
 
 # Create your views here.
 
 from  django.http import HttpResponse
 
 def index(request):
-    my_dictionary = {'insert_me': "Hello, this is views.py"}
-    return render(request, 'first_app/index.html', context=my_dictionary)
+    webpage_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_records' : webpage_list}
+    
+    return render(request, 'first_app/index.html', context=date_dict)
