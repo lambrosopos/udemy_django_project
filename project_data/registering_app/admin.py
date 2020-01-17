4339941dc1d30d43dbc130a8806ba2625a757daa
lambrosopos <lambrosopos@gmail.com>
2020-01-18 02:47:30 +0900

    Finally Submitting and saving onto db works

--- project_data/registering_app/admin.py
+++ project_data/registering_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from registering_app.models import Members
 
 # Register your models here.
+admin.site.register(Members)
--- project_data/registering_app/views.py
+++ project_data/registering_app/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+from registering_app.forms import NewMemberForm
 
 # Create your views here.
 def index(request):
     return render(request, "registering_app/index.html")
 
 def register(request):
-    return render(request, "registering_app/register.html")
+
+    form = NewMemberForm()
+
+    if request.method == "POST":
+        form = NewMemberForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print("ERROR FROM INVALID FORM")
+
+    return render(request, "registering_app/register.html", {"form":form})
