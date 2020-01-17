c736882697353a4274f5835a64c96cff51f2810f
lambrosopos <lambrosopos@gmail.com>
2020-01-18 04:46:04 +0900

    Done linking htmls, structured data, linked data and register page
    Notes : Still have to finish agreement checkbox

--- project_data/registering_app/models.py
+++ project_data/registering_app/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.core import validators
 # Create your models here.
+        
 class Members(models.Model):
     objects = models.Manager()
     firstName = models.CharField(max_length=254, unique=False)
     lastName = models.CharField(max_length=254, unique=False)
     email = models.EmailField(max_length=254, unique=True)
     password = models.CharField(max_length=254)
-    verifyPassword = models.CharField(max_length=254)
 
     def __str__(self):
         return f"{self.firstName} {self.lastName}"
