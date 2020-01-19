1518ddb83f26cfd292fa736d50d8d41abf7e44ed
lambrosopos <lambrosopos@gmail.com>
2020-01-19 23:09:50 +0900

    added password hashing methods

--- project_data/project_data/settings.py
+++ project_data/project_data/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
 
 PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.Argon2PasswordHasher',
-    'django.contrib.auth.hashers.BCryptSHA256PasswordHaser',
+    # 'django.contrib.auth.hashers.BCryptSHA256PasswordHaser',
     'django.contrib.auth.hashers.BCryptPasswordHasher',
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
     'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
--- project_data/registering_app/forms.py
+++ project_data/registering_app/forms.py
@@ -1,5 +1,19 @@
 from django import forms
-from registering_app.models import Members
+from django.contrib.auth.models import User
+from registering_app.models import Members, UserProfileInfo
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+class UserProfileInfoForm(forms.ModelForm):
+
+    class Meta:
+        model = UserProfileInfo
+        fields = ("portfolio_site", "profile_pic")
 
 class NewMemberForm(forms.ModelForm):
     verifyPassword = forms.CharField(widget=forms.PasswordInput)
