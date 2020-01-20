114b92f7d06d40a779d6791f8940a53ecdc998a4
lambrosopos <lambrosopos@gmail.com>
2020-01-20 14:55:01 +0900

    Completed : Login Page
    Notes : Haven't still linked to views

--- ttanttan/base_app/views.py
+++ ttanttan/base_app/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "base_app/index.html")
+
+
+def user_login(request):
+    return render(request, "base_app/login.html")
