203333c742d07779a2bd980a521980ea1e2fbede
lambrosopos <lambrosopos@gmail.com>
2020-02-07 22:45:35 +0900

    Updates
    Notes :
     - Added thank you and test pages for user login and logout
     - Fixed endblock issues

--- simplesocial/accounts/views.py
+++ simplesocial/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from accounts import forms
 from accounts import views
 
 # Create your views here.
